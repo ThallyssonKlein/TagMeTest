fc2ce1b175cca9b731fa0965a5c3273cf9ce24f0
Thallysson Klein <thallyssonklein@gmail.com>
2021-06-10 22:48:37 +0000

    Tests not passing

--- frontend/src/pages/login/index.test.js
+++ frontend/src/pages/login/index.test.js
@@ -2,16 +2,16 @@ import { screen } from "@testing-library/react";
 import { getPage } from 'next-page-tester';
 
 describe("Testing the login", _ => {
-    // test("should items be present in the document", async _ => {
-    //     const { render } = await getPage({
-    //         route: '/login',
-    //     });
-    //     render();      
+    test("should items be present in the document", async _ => {
+        const { render } = await getPage({
+            route: '/login',
+        });
+        render();      
 
-    //     expect(screen.getByPlaceholderText("Nome do usuário")).toBeInTheDocument();
-    //     expect(screen.getByPlaceholderText("Senha")).toBeInTheDocument();
-    //     expect(screen.getByText("Acessar")).toBeInTheDocument();
-    // }, 10000);
+        expect(screen.getByPlaceholderText("Nome do usuário")).toBeInTheDocument();
+        expect(screen.getByPlaceholderText("Senha")).toBeInTheDocument();
+        expect(screen.getByText("Acessar")).toBeInTheDocument();
+    }, 10000);
 
     it("should login with correct credentials", async _ => {
         const login = jest.mock('./index');
