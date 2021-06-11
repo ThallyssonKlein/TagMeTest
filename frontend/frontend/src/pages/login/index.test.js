4ecbef7a13d858e039e280e6ba61ae7b62026e8b
Thallysson Klein <thallyssonklein@gmail.com>
2021-06-11 04:53:23 +0000

    Backend tests are passing

--- frontend/frontend/src/pages/login/index.test.js
+++ frontend/frontend/src/pages/login/index.test.js
@@ -1,4 +1,4 @@
-import { screen, fireEvent, render } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import { getPage } from 'next-page-tester';
 import Login from './index';
 import ListOrders from '../listOrders';
@@ -18,48 +18,36 @@ describe("Testing the login", _ => {
 
     it("should login with correct credentials", async _ => {
         // render(<Login/>);
-        // const { render } = await getPage({
-        //     route : '/login'
-        // });
-        await act( async () => render(<Login/>));
-        // render();
+        const { render } = await getPage({
+            route : '/login'
+        });
+        render();
         const input1 = screen.getByPlaceholderText("Nome do usuário");
         const input2 = screen.getByPlaceholderText("Senha");
-        fireEvent.change(input1, { target: { value: 'test' } });
-        fireEvent.change(input2, { target: { value: 'test' } });
         const button = screen.getByText("Acessar");
-        fireEvent.click(button);
-
-        // const { render2 } = await getPage({
-        //     route: '/listOrders',
-        // });
-        await act( async () => render(<ListOrders/>));
-        // render2();
-        // render(<ListOrders/>);
+        await act(async _ => {
+            fireEvent.change(input1, { target: { value: 'test' } });
+            fireEvent.change(input2, { target: { value: 'test' } });
+            fireEvent.click(button);
+        });
 
         expect(screen.getByText("App")).toBeInTheDocument();
     });
 
     it("should not login with wrong credentials", async _ => {
         // render(<Login/>);
-        // const { render } = await getPage({
-        //     route : '/login'
-        // });
-        await act( async () => render(<Login/>));
-        // render();
+        const { render } = await getPage({
+            route : '/login'
+        });
+        render();
         const input1 = screen.getByPlaceholderText("Nome do usuário");
         const input2 = screen.getByPlaceholderText("Senha");
-        fireEvent.change(input1, { target: { value: 'test' } });
-        fireEvent.change(input2, { target: { value: 'test' } });
         const button = screen.getByText("Acessar");
-        fireEvent.click(button);
-
-        // const { render2 } = await getPage({
-        //     route: '/listOrders',
-        // });
-        await act( async () => render(<ListOrders/>));
-        // render2();
-        // render(<ListOrders/>);
+        await act(async _ => {
+            fireEvent.change(input1, { target: { value: 'test' } });
+            fireEvent.change(input2, { target: { value: 'test' } });
+            fireEvent.click(button);
+        });
 
         expect(screen.getByText("App")).not.toBeInTheDocument();
     });
--- orderservice/index.test.js
+++ orderservice/index.test.js
@@ -2,23 +2,30 @@ const request = require("supertest");
 const app = require("./index");
 
 describe("Orderservice api tests", _ => {
-    test("should GET all orders", _ => {
-        return request(app).get("/").then(response => {
-            expect(response.body).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({
-                        name : expect.any(String),
-                        photo : expect.any(String),
-                        description : expect.any(String),
-                        ingredients : expect.arrayContaining([
-                            expect.any(String)
-                        ]),
-                        steps : expect.arrayContaining([
-                            expect.any(String)
-                        ])
-                    })
-                ])
-            );
-        });
-    }, 1000000);
+    it('should GET all orders', function(done) {
+          request(app)
+          .get('/')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then(response => {
+                expect(response.body).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            name : expect.any(String),
+                            photo : expect.any(String),
+                            description : expect.any(String),
+                            ingredients : expect.arrayContaining([
+                                expect.any(String)
+                            ]),
+                            steps : expect.arrayContaining([
+                                expect.any(String)
+                            ])
+                        })
+                    ])
+                );
+              done();
+          })
+          .catch(err => done(err))
+      });
 });
